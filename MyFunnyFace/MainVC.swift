2bb8fb1cee34102df43ac5ab61fe3d0ffb369425
Jonathan Go <jogo88@gmail.com>
2018-07-24 11:20:47 +0900

    initial commit

---
+++ MyFunnyFace/MainVC.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MyFunnyFace
+//
+//  Created by Jonathan Go on 2018/07/24.
+//  Copyright © 2018 Appdelight. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+
+
+
+}
+
