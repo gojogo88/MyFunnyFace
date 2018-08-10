abe6081a9eb34e5edf64c37b6ddf703ee16c7692
Jonathan Go <jogo88@gmail.com>
2018-08-10 14:04:25 +0900

    main screen finished with animation but without language options yet.

--- MyFunnyFace/MainView.swift
+++ MyFunnyFace/MainView.swift
@@ -9,141 +9,186 @@
 import UIKit
 
 class MainView: UIView {
+  
+  let bgImageView: UIImageView = {
+    let iv = UIImageView()
+    iv.image = UIImage(named: "bgImage")
+    iv.contentMode = .scaleAspectFit
+    return iv
+  }()
+  
+  let newGameBtn: UIButton = {
+    let button = UIButton()
     
-    let bgImageView: UIImageView = {
-        let iv = UIImageView()
-        iv.image = UIImage(named: "bgImage")
-        iv.contentMode = .scaleAspectFit
-        return iv
-    }()
+    button.setImage(UIImage(named: "newGame"), for: .normal)
+    button.setAnchor(width: 0, height: 0)
+    return button
+  }()
+  
+  let newGameLabel: UILabel = {
+    let label = UILabel()
+    label.font = UIFont(name: "ChalkboardSE-Bold", size: 26)
+    label.text = "new game"
+    label.textColor = .myBlue
+    label.setAnchor(width: 0, height: 0)
+    return label
+  }()
+  
+  let galleryBtn: UIButton = {
+    let button = UIButton()
+    button.setImage(UIImage(named: "pictureFrame"), for: .normal)
+    button.setAnchor(width: 0, height: 0)
+    return button
+  }()
+  
+  let galleryLabel: UILabel = {
+    let label = UILabel()
+    label.font = UIFont(name: "ChalkboardSE-Bold", size: 26)
+    label.text = "gallery"
+    label.textColor = .myBlue
+    label.setAnchor(width: 0, height: 0)
+    return label
+  }()
+  
+  let introView: UIView = {
+    let narrView = UIView()
+    narrView.backgroundColor = .red
+    return narrView
+  }()
+  
+  let narratorPic: UIImageView = {
+    let iv = UIImageView()
+    iv.image = UIImage(named: "mouthClose")
+    iv.contentMode = .scaleAspectFit
+    return iv
+  }()
+  
+  
+  let chatBubbleBg: UIImageView = {
+    let iv = UIImageView()
+    iv.image = UIImage(named: "chatBubble")
+    iv.contentMode = .scaleAspectFit
+    iv.alpha = 0
+    return iv
+  }()
+  
+  let welcomeLabel: UILabel = {
+    let lbl = UILabel()
+    lbl.font = UIFont(name: "ChalkboardSE-Bold", size: 60)
+    lbl.textColor = .black
+    lbl.textAlignment = .center
+    lbl.text = ""
+    return lbl
+  }()
+  
+  lazy var newGameBtnBgView = ButtonBgView(button: newGameBtn, label: newGameLabel)
+  lazy var galleryBtnBgView = ButtonBgView(button: galleryBtn, label: galleryLabel)
+  
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
     
-    let newGameBtn: UIButton = {
-        let button = UIButton()
-        
-        button.setImage(UIImage(named: "newGame"), for: .normal)
-        button.setAnchor(width: 0, height: 0)
-        return button
-    }()
+    setup()
     
-    let newGameLabel: UILabel = {
-       let label = UILabel()
-        label.font = UIFont(name: "ChalkboardSE-Bold", size: 26)
-        label.text = "new game"
-        label.textColor = UIColor.myBlue
-        label.setAnchor(width: 0, height: 0)
-        return label
-    }()
+  }
+  
+  fileprivate func setup() {
     
-    let galleryBtn: UIButton = {
-        let button = UIButton()
-        button.setImage(UIImage(named: "pictureFrame"), for: .normal)
-        button.setAnchor(width: 0, height: 0)
-        return button
-    }()
+    var spacing: CGFloat!
+    var bottomHeight: CGFloat!
+    var narrWidth: CGFloat!
+    var narrHeight: CGFloat!
     
-    let galleryLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont(name: "ChalkboardSE-Bold", size: 26)
-        label.text = "gallery"
-        label.textColor = UIColor.myBlue
-        label.setAnchor(width: 0, height: 0)
-        return label
-    }()
-    
-    let narratorView: UIView = {
-        let view = UIView()
-        view.backgroundColor = UIColor.red
-        return view
-    }()
-    
-    let narratorPic: UIImageView = {
-        let iv = UIImageView()
-        iv.image = UIImage(named: "mouthClose")
-        iv.contentMode = .scaleAspectFit
-        return iv
-    }()
-    
-    let chatBubbleView: UIImageView = {
-        let iv = UIImageView()
-        iv.image = UIImage(named: "chatBubble")
-        iv.contentMode = .scaleAspectFit
-        return iv
-    }()
-    
-    lazy var newGameBtnBgView = ButtonBgView(button: newGameBtn, label: newGameLabel)
-    lazy var galleryBtnBgView = ButtonBgView(button: galleryBtn, label: galleryLabel)
-    
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        setup()
+    if UIDevice.whichDevice() == iOSDevice.isIpadPro129 {
+      spacing = 270
+      bottomHeight = 152
+      narrWidth = 1124
+      narrHeight = 174
+    } else {
+      spacing = 220
+      bottomHeight = 110
+      narrWidth = 840
+      narrHeight = 130
     }
     
-    fileprivate func setup() {
-        
-        var spacing: CGFloat!
-        var narrWidth: CGFloat!
-        var narrHeight: CGFloat!
-        var bubbleWidth: CGFloat!
-        var picWidth: CGFloat!
-        
-        if UIDevice.whichDevice() == iOSDevice.isIpadPro129 {
-            spacing = 270
-            narrWidth = 1124
-            narrHeight = 174
-            bubbleWidth = 942
-            picWidth = 232
-        } else if UIDevice.whichDevice() == iOSDevice.isIpadPro {
-            spacing = 220
-            narrWidth = 810
-            narrHeight = 130
-            bubbleWidth = 660
-            picWidth = 182
+    let stackView = mainStackView()
+    
+    self.addSubview(bgImageView)
+    bgImageView.setAnchor(top: self.topAnchor, left: self.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
+    
+    self.addSubview(introView)
+    introView.setAnchor(width: narrWidth, height: narrHeight)
+    introView.setAnchor(top: self.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 72, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
+    introView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+    
+    introView.addSubview(narratorPic)
+    narratorPic.setAnchor(top: introView.topAnchor, left: nil, bottom: introView.bottomAnchor, right: introView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
+    
+    introView.addSubview(chatBubbleBg)
+    chatBubbleBg.setAnchor(top: introView.topAnchor, left: introView.leftAnchor, bottom: introView.bottomAnchor, right: introView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 170)
+    
+    chatBubbleBg.addSubview(welcomeLabel)
+    welcomeLabel.setAnchor(top: chatBubbleBg.topAnchor, left: chatBubbleBg.leftAnchor, bottom: chatBubbleBg.bottomAnchor, right: chatBubbleBg.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 50)
+    
+    UIView.animate(withDuration: 0.1) {
+      self.introView.alpha = 0
+      self.chatBubbleBg.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+      self.introView.transform = CGAffineTransform(translationX: 0, y: -narrHeight)
+    }
+    
+    UIView.animate(withDuration: 1.5, delay: 1, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+      self.introView.alpha = 1
+      
+      self.introView.transform = .identity
+    }) { (finished) in
+      UIView.animate(withDuration: 1, delay: 0.2, options: [], animations: {
+        self.chatBubbleBg.alpha = 1
+        self.chatBubbleBg.transform =  .identity
+      }, completion: { (finished) in
+        self.animateWelcomeText()
+      })
+    }
+    
+    self.addSubview(stackView)
+    stackView.setAnchor(width: (self.frame.width - spacing), height: (self.frame.width - spacing) / 2.1)
+    stackView.setAnchor(top: nil, left: nil, bottom: bgImageView.bottomAnchor, right: nil, paddingTop: 94, paddingLeft: 0, paddingBottom: -bottomHeight, paddingRight: 0)
+    stackView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+  }
+  
+  func mainStackView() -> UIStackView {
+    let stackView = UIStackView(arrangedSubviews: [newGameBtnBgView, galleryBtnBgView])
+    stackView.axis = .horizontal
+    stackView.distribution = .fillProportionally
+    stackView.spacing = 20
+    
+    return stackView
+  }
+  
+  fileprivate func animateWelcomeText() {
+    let textToAnimate1 = "Welcome"
+    let textToAnimate2 = "Please make a selection"
+    
+    for char in textToAnimate1 {
+      welcomeLabel.text! += "\(char)"
+      RunLoop.current.run(until: Date()+0.18)
+    }
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1.1) {
+      
+      UIView.animate(withDuration: 1, animations: {
+        self.welcomeLabel.text = ""
+      }, completion: { (finished) in
+        for char in textToAnimate2 {
+          self.welcomeLabel.text! += "\(char)"
+          RunLoop.current.run(until: Date()+0.15)
         }
-        
-        let stackView = mainStackView()
-        
-        addSubview(bgImageView)
-        bgImageView.setAnchor(top: self.topAnchor, left: self.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
-        
-        setupNarrView(width: narrWidth, height: narrHeight, chatBubbleWidth: bubbleWidth, picWidth: picWidth)
-        addSubview(stackView)
-        stackView.setAnchor(width: (self.frame.width - spacing), height: (self.frame.width - spacing) / 2.1)
-        stackView.setAnchor(top: narratorView.bottomAnchor, left: nil, bottom: nil, right: nil, paddingTop: 94, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
-        stackView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        
+      })
+      
     }
-    
-    func mainStackView() -> UIStackView {
-        
-        let stackView = UIStackView(arrangedSubviews: [newGameBtnBgView, galleryBtnBgView])
-        stackView.axis = .horizontal
-        stackView.distribution = .fillProportionally
-        stackView.spacing = 20
-        
-        return stackView
-    }
-    
-    func setupNarrView(width: CGFloat, height: CGFloat, chatBubbleWidth: CGFloat, picWidth: CGFloat){
-        addSubview(narratorView)
-        narratorView.setAnchor(top: self.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 72, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
-        narratorView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        narratorView.setAnchor(width: width, height: height)
-        
-        addSubview(narratorPic)
-        narratorPic.setAnchor(width: picWidth, height: 0)
-        narratorPic.setAnchor(top: nil, left: nil, bottom: nil, right: narratorView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 2)
-        narratorPic.centerYAnchor.constraint(equalTo: narratorView.centerYAnchor).isActive = true
-        
-        addSubview(chatBubbleView)
-        chatBubbleView.setAnchor(width: chatBubbleWidth, height: 0)
-        chatBubbleView.setAnchor(top: narratorView.topAnchor, left: narratorView.leftAnchor, bottom: narratorView.bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 2, paddingBottom: 0, paddingRight: 0)
-    }
-    
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
 }
